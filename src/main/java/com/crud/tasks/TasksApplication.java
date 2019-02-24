695d6b1d2456d72b9e74bd95584a0a1ed01e4a0e
bulwahr <dawid.matlak@gmail.com>
2019-02-24 21:25:23 +0100

    Spring Web - REST API II (moduł 15)

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,19 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(TasksApplication.class, args);
+		SpringApplication.run(TasksApplication.class, args);
 
 
-		TaskDto taskDto = new TaskDto(
-				(long) 1,
-				"Test title",
-				"I want to check");
-
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
-
-		System.out.println(id + " " + title + " " + content);
+//		TaskDto taskDto = new TaskDto(
+//				(long) 1,
+//				"Test title",
+//				"I want to check");
+//
+//		Long id = taskDto.getId();
+//		String title = taskDto.getTitle();
+//		String content = taskDto.getContent();
+//
+//		System.out.println(id + " " + title + " " + content);
 
 	}
 }
--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.crud.tasks.repository;
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
@@ -11,5 +12,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     Task save(Task task);
 
-    Task findById(Long id);
+    Optional<Task> findById(Long id);
+
+    void deleteById(Long id);
 }
