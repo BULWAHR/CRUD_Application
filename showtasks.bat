e399e4f7bcba3ee1f3b5e50d013026bbd0c28e1e
bulwahr <dawid.matlak@gmail.com>
2019-03-01 17:39:01 +0100

    First attempt on Heroku

---
+++ showtasks.bat
@@ -0,0 +1,16 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto openBrowser
+echo.
+goto fail
+
+:openBrowser
+start chrome http://localhost:8080/crud/v1/task/getTasks
+goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
