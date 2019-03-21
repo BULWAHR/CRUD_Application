9f940656d10bc364b5ed53990bcc713ae91a1d41
bulwahr <dawid.matlak@gmail.com>
2019-03-21 17:39:36 +0100

    App startup fail

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,6 +1,5 @@
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,12 +7,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
-// public class TaskApplication {
+// public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TasksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TasksApplication.class, args);
+    }
 
 
 //		TaskDto taskDto = new TaskDto(
@@ -33,4 +32,3 @@ public class TasksApplication extends SpringBootServletInitializer {
 //	}
 
 }
-
